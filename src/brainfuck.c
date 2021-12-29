82083b64d5a400e2292125df6818aad6875efa87
MoizLokh <mlokhandwala360@gmail.com>
2021-12-29 11:23:32 -0800

    allow for file content to be copied into buffer

--- src/brainfuck.c
+++ src/brainfuck.c
@@ -1,27 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void bf_interpreter(char *buf, size_t size);
 
-void bf_interpreter(FILE *fptr) {
+void bf_interpreter(char *buf, size_t size) {
+  
   return;
 }
 
-int main(int argc, char *argv[]){
-
+int main(int argc, char *argv[]) {
+  FILE *fptr;
+  size_t fsize;
+  char *buffer;
+  
   if (argc < 2) {
     printf("Missing file argument \n");
-    return 0; 
+    exit(1); 
   }
   
-  FILE *fptr = fopen(argv[1], "r");
+  fptr = fopen(argv[1], "r");
   if (fptr == NULL) {
     printf("Failed to open file \n");
-    return 0;
+    exit(1);
   }
 
-  bf_interpreter(fptr);
-                                   
+  fseek(fptr, 0, SEEK_END);
+  fsize = ftell(fptr);
+  rewind(fptr);
+
+  // Initialize a buffer
+  buffer = malloc(fsize);
+  if (!buffer) {
+    printf("Failed to allocate memory");
+    free(buffer);
+    fclose(fptr);
+    exit(1);
+  }
+
+  // Add file contents into buffer
+  if (fread(buffer, fsize, 1, fptr) == 0) {
+    printf("Failed to read file");
+    free(buffer);
+    fclose(fptr);
+    exit(1);
+  }
+
   fclose(fptr);
+  //bf_interpreter(buffer);
+  free(buffer);
+  
   return 0;
 }
 
